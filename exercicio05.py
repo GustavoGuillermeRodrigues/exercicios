2c33fb80d7825259409b01439a09a04e164d9b01
gus-preto <0000119018453sp@al.educacao.sp.gov.br>
2025-05-06 12:08:07 -0300

    fizemos o sistema de cadastro junto com as notas

--- exercicio05.py
+++ exercicio05.py
@@ -1,12 +1,13 @@
-def cadastrar_aluno(nome, email, serie):
+def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     alunos = []
     aluno = {
         "nome": nome,
         "email": email,
         "serie": serie,
+        "nota": [nota01, nota02, nota03]
     }
 
     alunos.append(aluno)
     return alunos
-print(cadastrar_aluno ("gustavo", "gus@gmail.com", "2B"))
+print(cadastrar_aluno ("gustavo", "gus@gmail.com", "2°TB", 6, 8, 9))
 
