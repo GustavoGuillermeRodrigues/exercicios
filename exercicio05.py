dab1147be7b146c0e00cf1125036b9b44503129c
gus-preto <0000119018453sp@al.educacao.sp.gov.br>
2025-05-07 11:21:12 -0300

    sistema de nota junto ao sistema de media

--- exercicio05.py
+++ exercicio05.py
@@ -1,3 +1,5 @@
+from exercicio04 import somar_notas
+
 def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     alunos = []
     aluno = {
@@ -8,6 +10,7 @@ def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     }
 
     alunos.append(aluno)
+    media = somar_notas(aluno["nota"])
     return alunos
 print(cadastrar_aluno ("gustavo", "gus@gmail.com", "2°TB", 6, 8, 9))
 
