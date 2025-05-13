ddd687f9a0084cacbff21ddf00d5505ef88f0270
gus-preto <0000119018453sp@al.educacao.sp.gov.br>
2025-05-13 09:09:30 -0300

    atualisamos o sistema de cadatro adicionando informções com o sistema de codigo mais elaborado

--- exercicio05.py
+++ exercicio05.py
@@ -1,7 +1,18 @@
 from exercicio04 import somar_notas
 
+alunos = []
+
+def obter_dados():
+    nome_aluno = input("Informe o nome completo do aluno: ")
+    email_aluno = input("Informe o email do aluno: ")
+    serie_aluno = input("Informe a serie do aluno: ")
+    nota01 = float(input("Digite a nota01 do aluno: "))
+    nota02 = float(input("Digite a nota02 do aluno: "))
+    nota03 = float(input("Digite a nota03 do aluno: "))
+
+    return cadastrar_aluno(nome_aluno, email_aluno, serie_aluno, nota01, nota02, nota03)
+
 def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
-    alunos = []
 
     notas = [nota01, nota02, nota03]
 
@@ -16,5 +27,13 @@ def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
     alunos.append(aluno)
     media = somar_notas(aluno["nota"])
     return alunos
-print(cadastrar_aluno ("gustavo", "gus@gmail.com", "2°TB",8, 9, 8))
 
+obter_dados()
+
+def mostrar_dados(dados_alunos):
+    for aluno in dados_alunos:
+        print(f"Nome Do Aluno: {aluno["nome"]}")
+
+    return print(dados_alunos)
+
+mostrar_dados(alunos)
